26f93f1c56deb5054d4c5ef1f295074231c94b21
Abelit <ychenid@live.com>
2020-01-06 22:15:58 +0800

    add MyHTTPServer.java

---
+++ web/MyHTTPServer.java
@@ -0,0 +1,95 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+
+class SocketHandler implements Runnable {
+
+    final static String CRLF = "\r\n";   // 1
+
+    private Socket clientSocket;
+
+    public SocketHandler(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    public void handleSocket(Socket clientSocket) throws IOException {
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream())
+                );
+        PrintWriter out = new PrintWriter(
+                new BufferedWriter( new OutputStreamWriter(clientSocket.getOutputStream())),
+                true
+                );
+
+        String requestHeader = "";
+        String s;
+        while ((s = in.readLine()) != null) {
+            s += CRLF;
+            requestHeader = requestHeader + s;
+            if (s.equals(CRLF)){
+                break;
+            }
+        }
+        System.out.println("client request header:");
+        System.out.println(requestHeader);
+
+        String responseBody = "client request header: \n"+requestHeader;
+
+        String responseHeader = "HTTP/1.0 200 OK\r\n" +
+                "Content-Type: text/plain; charset=UTF-8\r\n" +
+                "Content-Length: "+responseBody.getBytes().length+"\r\n" +
+                "\r\n";
+        System.out.println("response: ");
+        System.out.println(responseHeader);
+
+        out.write(responseHeader);
+        out.write(responseBody);
+        out.flush();
+
+        out.close();
+        in.close();
+        clientSocket.close();
+
+    }
+
+    @Override
+    public void run() {
+        try {
+            handleSocket(clientSocket);
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
+
+public class MyHTTPServer {
+
+    public static void main(String[] args) throws Exception {
+
+        int port = 8000;
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("start server: " + port);
+
+        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(30); 
+
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("new join"
+                    + clientSocket.getInetAddress() + ":" + clientSocket.getPort());
+            try {
+                fixedThreadPool.execute(new SocketHandler(clientSocket));
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
